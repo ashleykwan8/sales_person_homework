ea7d9df9c3f9df09d9853eef8d30f69c0237e9fd
Ashley Kwan <ashleykwan888@gmail.com>
2020-10-06 19:26:56 -0700

    comment the for loop

--- sales_report.py
+++ sales_report.py
@@ -4,10 +4,10 @@
 salespeople = [] #empty list for salespeople
 melons_sold = [] #empty list for melons sold 
 
-f = open('sales-report.txt')
-for line in f:
-    line = line.rstrip()
-    entries = line.split('|')
+f = open('sales-report.txt')#oepn the sales-report.txt file
+for line in f: #loop through each line of the file
+    line = line.rstrip() #strip the whitespace 
+    entries = line.split('|') #split each element at the pipe line |
 
     salesperson = entries[0]
     melons = int(entries[2])
